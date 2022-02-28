9d661d77094d1eb386b18c82fe7da09cf83c2e14
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-02-27 21:28:48 -0800

    disable prebundleSvelteLibraries

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,6 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	experimental: {
-		prebundleSvelteLibraries: true
-	},
 	kit: {
 		adapter: adapter()
 	}
